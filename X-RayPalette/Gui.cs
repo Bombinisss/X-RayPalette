e0de43626f6b03a73c0968c19677d1a05a27e77a
Bombinisss <antekbom@gmail.com>
2024-03-05 22:48:27 +0100

    Initial commit with gui.

---
+++ X-RayPalette/Gui.cs
@@ -0,0 +1,101 @@
+﻿using System.Numerics;
+using ImGuiNET;
+
+namespace X_RayPalette
+{
+    public partial class Gui
+    {
+        private const ImGuiWindowFlags Flags = ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoMove |
+                                               ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.NoBackground;
+
+        private bool _isRunning;
+        private bool _loggedIn;
+        private string _username;
+        private string _password;
+        private string _passwordRepeat;
+
+        public Gui()
+        {
+            _isRunning = true;
+            _loggedIn = false;
+            _username = "";
+            _password = "";
+            _passwordRepeat = "";
+        }
+
+        public void SubmitUi()
+        {
+            ImGui.Begin("MedApp", ref _isRunning, Flags);
+            if (!_isRunning) Environment.Exit(0);
+
+            if (_loggedIn)
+            {
+                if (ImGui.BeginTabBar("TabBar", ImGuiTabBarFlags.FittingPolicyResizeDown))
+                {
+                    if (ImGui.BeginTabItem("My Patients"))
+                    {
+                        ImGui.BeginTable("Patients", 1);
+                        ImGui.EndTable();
+
+                        ImGui.EndTabItem();
+                    }
+
+                    if (ImGui.BeginTabItem("Add Patient"))
+                    {
+                        ImGui.EndTabItem();
+                    }
+                    
+                    if (ImGui.BeginTabItem("Dev")) //obviously will be moved in the future
+                    {
+                        //to do: select or drop image here and convert to long rainbow
+                        
+                        ImGui.EndTabItem();
+                    }
+                }
+            }
+            else
+            {
+                if (ImGui.BeginTabBar("TabBar", ImGuiTabBarFlags.FittingPolicyResizeDown))
+                {
+                    if (ImGui.BeginTabItem("Login"))
+                    {
+                        ImGui.Text("Username: ");
+                        ImGui.SameLine(0);
+                        ImGui.InputText("##username##", ref _username, 128);
+                        ImGui.Text("Password: ");
+                        ImGui.SameLine(0);
+                        ImGui.InputText("##passwd##", ref _password, 128, ImGuiInputTextFlags.Password);
+                        if (ImGui.Button("Login"))
+                        {
+                            //to do: check login
+                            _loggedIn = true;
+                        }
+
+                        ImGui.EndTabItem();
+                    }
+
+                    if (ImGui.BeginTabItem("Register"))
+                    {
+                        ImGui.Text("Username: ");
+                        ImGui.SameLine(95);
+                        ImGui.InputText("##username##", ref _username, 128);
+                        ImGui.Text("Password: ");
+                        ImGui.SameLine(95);
+                        ImGui.InputText("##passwd##", ref _password, 128, ImGuiInputTextFlags.Password);
+                        ImGui.Text("Confirm Pwd: ");
+                        ImGui.SameLine(95);
+                        ImGui.InputText("##passwd2##", ref _passwordRepeat, 128, ImGuiInputTextFlags.Password);
+
+                        // Register account
+                        if (ImGui.Button("Register"))
+                        {
+                            //to do: add to database
+                        }
+
+                        ImGui.EndTabItem();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ X-RayPalette/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using ImGuiNET;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace X_RayPalette
+{
+    public class Program
+    {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiRenderer _renderer;
+        private static readonly Vector3 _clearColor = new(0.0f, 0.0f, 0.0f);
+
+        private static void Main(string[] args)
+        {
+            // Create window, GraphicsDevice, and all resources necessary for the demo.
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 400, 150, WindowState.Normal, "Med App"),
+                new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+                out _window,
+                out _gd);
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _renderer.WindowResized(_window.Width, _window.Height);
+            };
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            _renderer = new ImGuiRenderer(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width,
+                _window.Height);
+
+            var stopwatch = Stopwatch.StartNew();
+            var deltaTime = 0f;
+            // Main application loop
+
+            var guiObject = new Gui();
+            ImGui.StyleColorsDark();
+            guiObject.SetupImGuiStyle();
+
+            while (_window.Exists)
+            {
+                deltaTime = stopwatch.ElapsedTicks / (float)Stopwatch.Frequency;
+                stopwatch.Restart();
+                var snapshot = _window.PumpEvents();
+                if (!_window.Exists) break;
+                _renderer.Update(deltaTime,
+                    snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                // ImGui window position and size
+                var viewport = ImGui.GetMainViewport();
+                ImGui.SetNextWindowPos(viewport.WorkPos);
+                ImGui.SetNextWindowSize(viewport.WorkSize);
+                guiObject.SubmitUi();
+
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+                _renderer.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            _gd.WaitForIdle();
+            _renderer.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
+        }
+    }
+}
