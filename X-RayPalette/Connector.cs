52170f9b01ba6678e1b0ac75945641b2eb27ad18
unknown <barejablazej@gmail.com>
2024-03-10 22:19:05 +0100

    Added database connection files

---
+++ X-RayPalette/Connector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace X_RayPalette
+{
+    internal class Connector
+    {
+        string connString = "server=localhost;user=root;database=project;port=3306;password=123$%;";
+
+
+
+    }
+}
+
+
+
