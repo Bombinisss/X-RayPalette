46b87db3838b4c2ee3a57ba5cf06da58f500a207
Bombinisss <antekbom@gmail.com>
2024-03-19 16:22:24 +0100

    Redesigned light theme.

--- X-RayPalette/ImguiTheme.cs
+++ X-RayPalette/ImguiTheme.cs
@@ -6,9 +6,10 @@ namespace X_RayPalette
     partial class Gui
     {
         private const int ThemeNumber = 1; //starting from 0
+
         public static void SetupImGuiStyle0()
         {
-            // Light styledougbinks from ImThemes
+            // Trash style from ImThemes
             var style = ImGui.GetStyle();
 
             style.Alpha = 1.0f;
@@ -19,12 +20,12 @@ namespace X_RayPalette
             style.WindowMinSize = new Vector2(32.0f, 32.0f);
             style.WindowTitleAlign = new Vector2(0.0f, 0.5f);
             style.WindowMenuButtonPosition = ImGuiDir.Left;
-            style.ChildRounding = 0.0f;
+            style.ChildRounding = 6.900000095367432f;
             style.ChildBorderSize = 1.0f;
-            style.PopupRounding = 0.0f;
+            style.PopupRounding = 6.900000095367432f;
             style.PopupBorderSize = 1.0f;
             style.FramePadding = new Vector2(4.0f, 3.0f);
-            style.FrameRounding = 0.0f;
+            style.FrameRounding = 6.900000095367432f;
             style.FrameBorderSize = 0.0f;
             style.ItemSpacing = new Vector2(8.0f, 4.0f);
             style.ItemInnerSpacing = new Vector2(4.0f, 4.0f);
@@ -32,10 +33,10 @@ namespace X_RayPalette
             style.IndentSpacing = 21.0f;
             style.ColumnsMinSpacing = 6.0f;
             style.ScrollbarSize = 14.0f;
-            style.ScrollbarRounding = 9.0f;
+            style.ScrollbarRounding = 6.900000095367432f;
             style.GrabMinSize = 10.0f;
-            style.GrabRounding = 0.0f;
-            style.TabRounding = 4.0f;
+            style.GrabRounding = 6.900000095367432f;
+            style.TabRounding = 6.900000095367432f;
             style.TabBorderSize = 0.0f;
             style.TabMinWidthForCloseButton = 0.0f;
             style.ColorButtonPosition = ImGuiDir.Right;
@@ -46,23 +47,23 @@ namespace X_RayPalette
             style.Colors[(int)ImGuiCol.TextDisabled] =
                 new Vector4(0.6000000238418579f, 0.6000000238418579f, 0.6000000238418579f, 1.0f);
             style.Colors[(int)ImGuiCol.WindowBg] =
-                new Vector4(0.9372549057006836f, 0.9372549057006836f, 0.9372549057006836f, 1.0f);
+                new Vector4(0.8369098901748657f, 0.8369014859199524f, 0.8369014859199524f, 1.0f);
             style.Colors[(int)ImGuiCol.ChildBg] = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
             style.Colors[(int)ImGuiCol.PopupBg] = new Vector4(1.0f, 1.0f, 1.0f, 0.9800000190734863f);
             style.Colors[(int)ImGuiCol.Border] = new Vector4(0.0f, 0.0f, 0.0f, 0.300000011920929f);
             style.Colors[(int)ImGuiCol.BorderShadow] = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
-            style.Colors[(int)ImGuiCol.FrameBg] = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-            style.Colors[(int)ImGuiCol.FrameBgHovered] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.FrameBg] =
+                new Vector4(0.8927038908004761f, 0.8926949501037598f, 0.8926949501037598f, 1.0f);
+            style.Colors[(int)ImGuiCol.FrameBgHovered] = new Vector4(0.6407473683357239f, 0.2588235437870026f,
                 0.9764705896377563f, 0.4000000059604645f);
-            style.Colors[(int)ImGuiCol.FrameBgActive] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.FrameBgActive] = new Vector4(0.6037868857383728f, 0.2588235437870026f,
                 0.9764705896377563f, 0.6700000166893005f);
             style.Colors[(int)ImGuiCol.TitleBg] =
                 new Vector4(0.95686274766922f, 0.95686274766922f, 0.95686274766922f, 1.0f);
             style.Colors[(int)ImGuiCol.TitleBgActive] =
                 new Vector4(0.8196078538894653f, 0.8196078538894653f, 0.8196078538894653f, 1.0f);
             style.Colors[(int)ImGuiCol.TitleBgCollapsed] = new Vector4(1.0f, 1.0f, 1.0f, 0.5099999904632568f);
-            style.Colors[(int)ImGuiCol.MenuBarBg] =
-                new Vector4(0.8588235378265381f, 0.8588235378265381f, 0.8588235378265381f, 1.0f);
+            style.Colors[(int)ImGuiCol.MenuBarBg] = new Vector4(1.0f, 0.9999899864196777f, 0.9999899864196777f, 1.0f);
             style.Colors[(int)ImGuiCol.ScrollbarBg] = new Vector4(0.9764705896377563f, 0.9764705896377563f,
                 0.9764705896377563f, 0.5299999713897705f);
             style.Colors[(int)ImGuiCol.ScrollbarGrab] = new Vector4(0.686274528503418f, 0.686274528503418f,
@@ -72,51 +73,51 @@ namespace X_RayPalette
             style.Colors[(int)ImGuiCol.ScrollbarGrabActive] =
                 new Vector4(0.4862745106220245f, 0.4862745106220245f, 0.4862745106220245f, 1.0f);
             style.Colors[(int)ImGuiCol.CheckMark] =
-                new Vector4(0.2588235437870026f, 0.5882353186607361f, 0.9764705896377563f, 1.0f);
-            style.Colors[(int)ImGuiCol.SliderGrab] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
-                0.9764705896377563f, 0.7799999713897705f);
-            style.Colors[(int)ImGuiCol.SliderGrabActive] = new Vector4(0.4588235318660736f, 0.5372549295425415f,
-                0.800000011920929f, 0.6000000238418579f);
-            style.Colors[(int)ImGuiCol.Button] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+                new Vector4(0.4948495924472809f, 0.2599999904632568f, 0.9800000190734863f, 1.0f);
+            style.Colors[(int)ImGuiCol.SliderGrab] =
+                new Vector4(0.4322747886180878f, 0.2400000393390656f, 0.8799999952316284f, 1.0f);
+            style.Colors[(int)ImGuiCol.SliderGrabActive] =
+                new Vector4(0.4577679634094238f, 0.2599999904632568f, 0.9800000190734863f, 1.0f);
+            style.Colors[(int)ImGuiCol.Button] = new Vector4(0.5113858580589294f, 0.2588235437870026f,
                 0.9764705896377563f, 0.4000000059604645f);
             style.Colors[(int)ImGuiCol.ButtonHovered] =
-                new Vector4(0.2588235437870026f, 0.5882353186607361f, 0.9764705896377563f, 1.0f);
+                new Vector4(0.6222667694091797f, 0.2588235437870026f, 0.9764705896377563f, 1.0f);
             style.Colors[(int)ImGuiCol.ButtonActive] =
-                new Vector4(0.05882352963089943f, 0.529411792755127f, 0.9764705896377563f, 1.0f);
-            style.Colors[(int)ImGuiCol.Header] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
-                0.9764705896377563f, 0.3100000023841858f);
-            style.Colors[(int)ImGuiCol.HeaderHovered] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+                new Vector4(0.452663391828537f, 0.05882354825735092f, 0.9764705896377563f, 1.0f);
+            style.Colors[(int)ImGuiCol.Header] =
+                new Vector4(0.4936792254447937f, 0.2489270567893982f, 1.0f, 0.3100000023841858f);
+            style.Colors[(int)ImGuiCol.HeaderHovered] = new Vector4(0.5113858580589294f, 0.2588235437870026f,
                 0.9764705896377563f, 0.800000011920929f);
             style.Colors[(int)ImGuiCol.HeaderActive] =
-                new Vector4(0.2588235437870026f, 0.5882353186607361f, 0.9764705896377563f, 1.0f);
+                new Vector4(0.5298660397529602f, 0.2588235437870026f, 0.9764705896377563f, 1.0f);
             style.Colors[(int)ImGuiCol.Separator] = new Vector4(0.3882353007793427f, 0.3882353007793427f,
                 0.3882353007793427f, 0.6200000047683716f);
-            style.Colors[(int)ImGuiCol.SeparatorHovered] = new Vector4(0.1372549086809158f, 0.4392156898975372f,
+            style.Colors[(int)ImGuiCol.SeparatorHovered] = new Vector4(0.3875619471073151f, 0.1372549086809158f,
                 0.800000011920929f, 0.7799999713897705f);
             style.Colors[(int)ImGuiCol.SeparatorActive] =
-                new Vector4(0.1372549086809158f, 0.4392156898975372f, 0.800000011920929f, 1.0f);
+                new Vector4(0.4387612342834473f, 0.1372549086809158f, 0.800000011920929f, 1.0f);
             style.Colors[(int)ImGuiCol.ResizeGrip] = new Vector4(0.3490196168422699f, 0.3490196168422699f,
                 0.3490196168422699f, 0.1700000017881393f);
-            style.Colors[(int)ImGuiCol.ResizeGripHovered] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.ResizeGripHovered] = new Vector4(0.5113858580589294f, 0.2588235437870026f,
                 0.9764705896377563f, 0.6700000166893005f);
-            style.Colors[(int)ImGuiCol.ResizeGripActive] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.ResizeGripActive] = new Vector4(0.585306704044342f, 0.2588235437870026f,
                 0.9764705896377563f, 0.949999988079071f);
-            style.Colors[(int)ImGuiCol.Tab] = new Vector4(0.7607843279838562f, 0.7960784435272217f, 0.8352941274642944f,
+            style.Colors[(int)ImGuiCol.Tab] = new Vector4(0.7927627563476562f, 0.7607843279838562f, 0.8352941274642944f,
                 0.9309999942779541f);
-            style.Colors[(int)ImGuiCol.TabHovered] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.TabHovered] = new Vector4(0.4929056763648987f, 0.2588235437870026f,
                 0.9764705896377563f, 0.800000011920929f);
             style.Colors[(int)ImGuiCol.TabActive] =
-                new Vector4(0.5921568870544434f, 0.7254902124404907f, 0.8823529481887817f, 1.0f);
-            style.Colors[(int)ImGuiCol.TabUnfocused] = new Vector4(0.9176470637321472f, 0.9254902005195618f,
+                new Vector4(0.7092316746711731f, 0.5921568870544434f, 0.8823529481887817f, 1.0f);
+            style.Colors[(int)ImGuiCol.TabUnfocused] = new Vector4(0.9243793487548828f, 0.9176470637321472f,
                 0.9333333373069763f, 0.9861999750137329f);
             style.Colors[(int)ImGuiCol.TabUnfocusedActive] =
-                new Vector4(0.7411764860153198f, 0.8196078538894653f, 0.9137254953384399f, 1.0f);
+                new Vector4(0.8241184949874878f, 0.741176426410675f, 0.9137254953384399f, 1.0f);
             style.Colors[(int)ImGuiCol.PlotLines] =
                 new Vector4(0.3882353007793427f, 0.3882353007793427f, 0.3882353007793427f, 1.0f);
             style.Colors[(int)ImGuiCol.PlotLinesHovered] =
                 new Vector4(1.0f, 0.4274509847164154f, 0.3490196168422699f, 1.0f);
             style.Colors[(int)ImGuiCol.PlotHistogram] =
-                new Vector4(0.8980392217636108f, 0.6980392336845398f, 0.0f, 1.0f);
+                new Vector4(0.0f, 0.606256365776062f, 0.8025751113891602f, 1.0f);
             style.Colors[(int)ImGuiCol.PlotHistogramHovered] = new Vector4(1.0f, 0.4470588266849518f, 0.0f, 1.0f);
             style.Colors[(int)ImGuiCol.TableHeaderBg] =
                 new Vector4(0.7764706015586853f, 0.8666666746139526f, 0.9764705896377563f, 1.0f);
@@ -127,11 +128,11 @@ namespace X_RayPalette
             style.Colors[(int)ImGuiCol.TableRowBg] = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
             style.Colors[(int)ImGuiCol.TableRowBgAlt] = new Vector4(0.2980392277240753f, 0.2980392277240753f,
                 0.2980392277240753f, 0.09000000357627869f);
-            style.Colors[(int)ImGuiCol.TextSelectedBg] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.TextSelectedBg] = new Vector4(0.5113858580589294f, 0.2588235437870026f,
                 0.9764705896377563f, 0.3499999940395355f);
-            style.Colors[(int)ImGuiCol.DragDropTarget] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.DragDropTarget] = new Vector4(0.41898512840271f, 0.2588235437870026f,
                 0.9764705896377563f, 0.949999988079071f);
-            style.Colors[(int)ImGuiCol.NavHighlight] = new Vector4(0.2588235437870026f, 0.5882353186607361f,
+            style.Colors[(int)ImGuiCol.NavHighlight] = new Vector4(0.5298660397529602f, 0.2588235437870026f,
                 0.9764705896377563f, 0.800000011920929f);
             style.Colors[(int)ImGuiCol.NavWindowingHighlight] = new Vector4(0.6980392336845398f, 0.6980392336845398f,
                 0.6980392336845398f, 0.699999988079071f);
