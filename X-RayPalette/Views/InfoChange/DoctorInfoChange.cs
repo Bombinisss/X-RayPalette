25c26e45cbb65d54299b2798c5a0fffb841fb2d3
unknown <barejablazej@gmail.com>
2024-05-09 20:43:20 +0200

    base of DoctorChangeInfo View done, there is a problem with a function GetStringListFromExecSql it only returns List containing 1 element

--- X-RayPalette/Views/InfoChange/DoctorInfoChange.cs
+++ X-RayPalette/Views/InfoChange/DoctorInfoChange.cs
@@ -3,6 +3,7 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -13,40 +14,9 @@ using X_RayPalette.Helpers;
 
 namespace X_RayPalette.Views.InfoChange
 {
-    public delegate void ComboChangedHandler(string _tempdataDocCi);
-    class EventProgram
-    {
-        event ComboChangedHandler ComboChanged;
-        public EventProgram()
-        {
-            this.ComboChanged += new ComboChangedHandler(this.HandleComboChanged);
-        }
-        public void HandleComboChanged(string _tempdataDocChange)
-        {
-            Console.WriteLine(_tempdataDocChange);
 
-
-
-        }
-
-    }
     internal class DoctorInfoChange : View
     {
-        EventProgram EventProgram = new EventProgram();
-        public string _tempdataDocChange
-        {
-            get
-            {
-                return _tempdataDocCi;
-            }
-            set
-            {
-                _tempdataDocCi = value;
-                EventProgram.HandleComboChanged(_tempdataDocCi);
-            }
-
-        }
-
         private string _tempdataDocCi;
 
         private string _updateDoctorName;
@@ -71,14 +41,13 @@ namespace X_RayPalette.Views.InfoChange
             _updateDoctorEmail = "";
             _updateDoctorPhone = "";
             _updateDoctorPhoneAreaCode = InputDataHelper.PhoneAreaCodes.First();
-
             _updatePasswordRepeat = "";
             _updateUsernameRegister = "";
             _updatePasswordRegister = "";
         }
         public override void Back()
         {
-            _tempdataDocCi = "Choose Doctor";
+            _tempdataDocCi = "";
             OnBackEvent();
         }
 
@@ -94,78 +63,115 @@ namespace X_RayPalette.Views.InfoChange
             }
             _nameArray.ToArray();
             reader.Close();
-            new ComboBox<string>(_tempdataDocCi,"##DocInfoChange##", _nameArray).OnSelect((string val) =>
+            string old,n;
+            new ComboBox<string>(_tempdataDocCi, "##DocInfoChange##", _nameArray).OnSelect((string _selectedDocString) => _tempdataDocCi = _selectedDocString)
+            .OnValueChange((old, n)=>
             {
-                _tempdataDocCi = val;
-                //event
+                string[] _selectedDocIdName = n.Split(' ');
+                try
+                {
+                    string _selectedDocId = _selectedDocIdName[0];
+                    Console.WriteLine(_selectedDocId);
+                    List<string> MainReader = Program.dbService.GetStringListFromExecSql("Select first_name, sur_name, sex, PESEL, email, phone from doctors where doctors_id='" + _selectedDocId + "';");
+                    string[] _data = MainReader.ToArray();
+                    _updateDoctorName = _data[0];
+                    _updateDoctorSurname = _data[1];
+                    _updateDoctorSex = Convert.ToInt32(_data[2]);
+                    _updateDoctorPesel = _data[3];
+                    _updateDoctorEmail = _data[4];
+                    _updateDoctorPhone = _data[5];
+                    _updatePasswordRepeat = "";
+                    _updateUsernameRegister = "";
+                    _updatePasswordRegister = "";
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex);
+                    _tempdataDocCi = "Choose Doctor";
+                    _updateDoctorName = "";
+                    _updateDoctorSurname = "";
+                    _updateDoctorSex = 1;
+                    _updateDoctorPesel = "";
+                    _updateDoctorEmail = "";
+                    _updateDoctorPhone = "";
+                    _updateDoctorPhoneAreaCode = InputDataHelper.PhoneAreaCodes.First();
+                    _updatePasswordRepeat = "";
+                    _updateUsernameRegister = "";
+                    _updatePasswordRegister = "";
+
+                }
             }).Render();
 
-            ImGui.Separator();
-            if (_tempdataDocCi != "Choose Doctor")
-            {
 
-                ImGui.Text("Name: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##Doctorname##", ref _updateDoctorName, 128);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-
-                ImGui.Text("Surname: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##Doctorsurname##", ref _updateDoctorSurname, 128);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-
-                ImGui.Text("Sex: ");
-                ImGui.SameLine(110);
-
-                ImGui.RadioButton("Men", ref _updateDoctorSex, 1);
-                ImGui.SameLine(200);
-                ImGui.RadioButton("Woman", ref _updateDoctorSex, 2);
-
-                ImGui.Text("PESEL: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##Doctorpesel##", ref _updateDoctorPesel, 11);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-
-                ImGui.Text("E-mail: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##Doctoremailadress##", ref _updateDoctorEmail, 128);
-
-                ImGui.Text("Phone: ");
-                ImGui.SameLine(110);
-                new ComboBox<PhoneAreaCode>(_updateDoctorPhoneAreaCode,"##DoctorphoneArea", InputDataHelper.PhoneAreaCodes).Width(50).OnSelect((x) => _updateDoctorPhoneAreaCode = x).Render();
-                ImGui.SameLine(0);
-                ImGui.PushItemWidth(150);
-                ImGui.InputText("##Doctorphone##", ref _updateDoctorPhone, 15, ImGuiInputTextFlags.CharsDecimal);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A - required field");
 
                 ImGui.Separator();
-                ImGui.Text("Username: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##usernameReg##", ref _updateUsernameRegister, 128);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-                ImGui.Text("Password: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##passwdReg##", ref _updatePasswordRegister, 128, ImGuiInputTextFlags.Password);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
-                ImGui.Text("Confirm Pwd: ");
-                ImGui.SameLine(110);
-                ImGui.InputText("##passwd2##", ref _updatePasswordRepeat, 128, ImGuiInputTextFlags.Password);
-                ImGui.SameLine();
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+                if (_tempdataDocCi != "Choose Doctor")
+                {
 
-                ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A - required field");
-                ImGui.Separator();
-            }
+                    ImGui.Text("Name: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##Doctorname##", ref _updateDoctorName, 128);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
 
-            new Button("Confirm changes").OnClick(Back).Render();
+                    ImGui.Text("Surname: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##Doctorsurname##", ref _updateDoctorSurname, 128);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+
+                    ImGui.Text("Sex: ");
+                    ImGui.SameLine(110);
+
+                    ImGui.RadioButton("Men", ref _updateDoctorSex, 1);
+                    ImGui.SameLine(200);
+                    ImGui.RadioButton("Woman", ref _updateDoctorSex, 2);
+
+                    ImGui.Text("PESEL: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##Doctorpesel##", ref _updateDoctorPesel, 11);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+
+                    ImGui.Text("E-mail: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##Doctoremailadress##", ref _updateDoctorEmail, 128);
+
+                    ImGui.Text("Phone: ");
+                    ImGui.SameLine(110);
+                    new ComboBox<PhoneAreaCode>(_updateDoctorPhoneAreaCode, "##DoctorphoneArea", InputDataHelper.PhoneAreaCodes).Width(50).OnSelect((x) => _updateDoctorPhoneAreaCode = x).Render();
+                    ImGui.SameLine(0);
+                    ImGui.PushItemWidth(150);
+                    ImGui.InputText("##Doctorphone##", ref _updateDoctorPhone, 15, ImGuiInputTextFlags.CharsDecimal);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A - required field");
+
+                    ImGui.Separator();
+                    ImGui.Text("Username: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##usernameReg##", ref _updateUsernameRegister, 128);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+                    ImGui.Text("Password: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##passwdReg##", ref _updatePasswordRegister, 128, ImGuiInputTextFlags.Password);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+                    ImGui.Text("Confirm Pwd: ");
+                    ImGui.SameLine(110);
+                    ImGui.InputText("##passwd2##", ref _updatePasswordRepeat, 128, ImGuiInputTextFlags.Password);
+                    ImGui.SameLine();
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A");
+
+                    ImGui.TextColored(new Vector4(0.8f, 0.20f, 0.20f, 0.90f), "\u002A - required field");
+                    ImGui.Separator();
+                }
+
+                new Button("Confirm changes").OnClick(Back).Render();
 
 
         }
